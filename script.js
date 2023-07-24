abcc9e6ae7452bc8cd50c3de4eee7bca5f1a9c94
Dawid <dawid130892@interia.pl>
2023-07-24 18:46:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const ball = document.querySelector(".ball-img");
+const question = document.querySelector("input");
+const answer = document.querySelector(".answer");
+const error = document.querySelector(".error");
+
+const anwer_1 = "Glupie pytanie, zadaj inne!";
+const answr_2 = "Na pewno chcesz wiedziec? Chyba jednak Ci podaruje i nie odpowiem";
+const answer_3= "Nie chcesz znac odpowiedz na to pytanie, zaufaj mi!";
+const answer_4 = {}
