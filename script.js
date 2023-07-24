97843937ed65a17fa7a12309e9ad4c7c243efe98
Dawid <dawid130892@interia.pl>
2023-07-24 20:35:40 +0200

    Added some more answers

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const answer_5 = "Zdecydowanie nie";
 const answer_6 = "Ale ze o co Ci wlasciwie chodzi?";
 const answer_7 = "A wez sobie chataGPT zapytaj";
 const answer_8 = "A po co Ci to wlasciwie wiedziec co?";
+const answer_9 = "Beznadzieja.. zapytaj o cos ciekawszego";
+const answer_10 = "Tobie to serio brak 5 klepki";
+const answer_11 = "A jak myslisz?";
+const answer_12 = "No wez, serio? Nie no, kurde, nie wiem..";
+const answer_13 = "Nie rozumiem pytania";
 const answerArray = [
 	answer_1,
 	answer_2,
@@ -20,13 +25,18 @@ const answerArray = [
 	answer_6,
 	answer_7,
 	answer_8,
+	answer_9,
+	answer_10,
+	answer_11,
+	answer_12,
+	answer_13,
 ];
 
 function answerHandler(text) {
-    ball.classList.add("rotate-animation");
-    setTimeout(() => {
-        ball.classList.remove("rotate-animation");
-      }, 1000);
+	ball.classList.add("rotate-animation");
+	setTimeout(() => {
+		ball.classList.remove("rotate-animation");
+	}, 1000);
 	function getRandomIndex(answerArray) {
 		const maxIndex = answerArray.length - 1;
 		return Math.floor(Math.random() * (maxIndex + 1));
@@ -42,8 +52,7 @@ function answerHandler(text) {
 	} else {
 		text = randomElement;
 	}
-	answer.innerHTML=text;
-    
+	answer.innerHTML = text;
 }
 
 ball.addEventListener("click", answerHandler);
